35241fef2738566b173575de53fa81973b5cbd1b
mlidrbauch <margaret.lidrbauch@students.olin.edu>
2015-04-16 15:08:01 -0400

    Model with some WF runs
    HA N_c of 7.6%. LOL.

--- OTECmainFunction.m
+++ OTECmainFunction.m
@@ -13,15 +13,27 @@
 % of the cycle processes. The pump and turbine operate isentropically. No
 % kinetic or potential energy effects. 
 
-function [Wnet_m] = WAH10_2 (T1,T3,q1,q3,fluidName)
+function [Wnet_m, N, N_c] = WAH10_2 (T1,T3,q1,q3,fluidName)
     % returns P1 in [kPA], H1 in [J/kg], and S1 in [J/kgK]
     [P1,H1,S1] = refpropm('PHS','T',T1,'Q',q1,fluidName); %sets state 1
     [P3,H3,S3] = refpropm('PHS','T',T3,'Q',q3,fluidName); %sets state 3
     [T2,Q2,H2] = refpropm('TQH','P',P3,'S',S1,fluidName); %sets state 2
     [T4,Q4,H4] = refpropm('TQH','P',P1,'S',S3,fluidName); %sets state 4
     
+    %P1
+    %P3
     Wt_m = H1-H2; %[J/kg]
     Wp_m = H3-H4; %[J/kg]
     Wnet_m = Wt_m + Wp_m; %[J/kg]
+    
+    Qin_m = H1-H4; %[J/kg]
+    N = Wnet_m/Qin_m;
+    N_c = 1-T3/T1;
+    %with ammonia: 91,184 J/kg
+    %with R134a: 14,416 J/kg
+    %with R22: 14,609 J/kg
+    %with water: 188,220 J/kg something wrong? Probably. Operates between 1
+    %and 4 kPa, 7% efficiency
+    %Carnot Efficiency of cycle is 7.59%
     
 end
