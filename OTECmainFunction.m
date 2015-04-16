500dca21a5207beb4dc3c17c2b73913d2883b596
mlidrbauch <margaret.lidrbauch@students.olin.edu>
2015-04-16 14:46:49 -0400

    Set all states

--- OTECmainFunction.m
+++ OTECmainFunction.m
@@ -4,7 +4,11 @@
 
 
 %% Iteration 1 - Closed System
-function [Wc] = WAH10_2 (T1,q1,fluidName)
+function [Wc] = WAH10_2 (T1,T3,q1,q3,fluidName)
     % returns P1 in [kPA], H1 in [J/kg], and S1 in [J/kgK]
-    [P1,H1,S1] = refpropm('PHS','T',T1,'Q',q1,fluidName)
+    [P1,H1,S1] = refpropm('PHS','T',T1,'Q',q1,fluidName); %sets state 1
+    [P3,H3,S3] = refpropm('PHS','T',T3,'Q',q3,fluidName); %sets state 3
+    [T2,Q2,H2] = refpropm('TQH','P',P3,'S',S1,fluidName); %sets state 2
+    [T3,Q3,H3] = refpropm('TQH','P',P1,'S',S3,fluidName); %sets state 4
+    
 end
