f89edf8ea77f96279dc13d78e0bbc302c64a40a9
SKaragianes <sean.karagianes@students.olin.edu>
2015-04-16 14:16:14 -0400

    Added Date 2

--- OTECmainFunction.m
+++ OTECmainFunction.m
@@ -1,2 +1,3 @@
 %% THERMO OTEC PROJECT
-% Lindsey Andrade, Sean Karagianes, Meg Lidrbauch% Lindsey Andrade, Sean Karagianes, Meg Lidrbauch
+% Lindsey Andrade, Sean Karagianes, Meg Lidrbauch
+% Spring 2015
