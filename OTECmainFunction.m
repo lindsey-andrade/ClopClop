5d19b09b6b2bbb30a0fad7666e5fa332ee931e64
mlidrbauch <margaret.lidrbauch@students.olin.edu>
2015-04-16 14:42:34 -0400

    Added Prelim function to set first state

--- OTECmainFunction.m
+++ OTECmainFunction.m
@@ -4,4 +4,7 @@
 
 
 %% Iteration 1 - Closed System
-
+function [Wc] = WAH10_2 (T1,q1,fluidName)
+    % returns P1 in [kPA], H1 in [J/kg], and S1 in [J/kgK]
+    [P1,H1,S1] = refpropm('PHS','T',T1,'Q',q1,fluidName)
+end
